090f1ede7a7023b9d4c2f5a159d1d1204a51c752
Saviour1001 <harshghodkar@gmail.com>
2023-08-31 19:26:20 +0530

    changed networks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-import { WagmiConfig, createConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
-import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
-import Layout from '../components/Layout';
+import { WagmiConfig, createConfig } from "wagmi";
+import { fantomTestnet, avalancheFuji, polygonMumbai } from "wagmi/chains";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import Layout from "../components/Layout";
 
 const config = createConfig(
   getDefaultConfig({
-    appName: 'ConnectKit Next.js demo',
+    appName: "ConnectKit Next.js demo",
     //infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
     //alchemyId:  process.env.NEXT_PUBLIC_ALCHEMY_ID,
-    chains: [mainnet, polygon, optimism, arbitrum],
+    chains: [polygonMumbai, avalancheFuji, fantomTestnet],
     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
   })
 );
